55d5fe9d7bcfdb87922e63f9b27ad89ec04c444d
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-04-14 06:16:44 +0300

    Create localization.ruRU.lua

---
+++ locals/localization.ruRU.lua
@@ -0,0 +1,36 @@
+-- Translator ZamestoTV
+local _, accountGoldTracker = ...
+
+if GetLocale() ~= "ruRU" then return end
+
+local L = accountGoldTracker.localization
+
+L["January"] = "Январь"
+L["February"] = "Февраль"
+L["March"] = "Март"
+L["April"] = "Апрель"
+L["May"] = "Май"
+L["June"] = "Июнь"
+L["July"] = "Июль"
+L["August"] = "Август"
+L["September"] = "Сентябрь"
+L["October"] = "Октябрь"
+L["November"] = "Ноябрь"
+L["December"] = "Декабрь"
+
+L["button-next"] = "Вперед"
+L["button-previous"] = "Назад"
+
+L["date"] = "Дата"
+L["amount"] = "Сумма"
+L["difference"] = "Разница"
+
+L["no-entries"] = "Нет записей за этот месяц."
+
+L["general-options"] = "Основные настройки"
+L["open-on-login.name"] = "Открывать обзор золота при входе"
+L["open-on-login.tooltip"] = "Активирует или деактивирует автоматическое открытие обзора золота при входе в игру."
+
+L["other-options"] = "Другие настройки"
+L["debug.name"] = "Включить режим отладки"
+L["debug.tooltip"] = "Активирует или деактивирует режим отладки."
