5343c688fa8619516ec6f4ed8e1f75659b20f70d
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-04-15 02:47:56 +0500

    Create localization.ruRU.lua

---
+++ locals/localization.ruRU.lua
@@ -0,0 +1,51 @@
+local _, goldCurrencyTracker = ...
+
+if GetLocale() ~= "ruRU" then return end
+
+local L = goldCurrencyTracker.localization
+
+L["jan"] = "Январь"
+L["feb"] = "Февраль"
+L["mar"] = "Март "
+L["apr"] = "Апрель"
+L["may"] = "Май"
+L["jun"] = "Июнь"
+L["jul"] = "Июль"
+L["aug"] = "Август"
+L["sep"] = "Сентябрь"
+L["oct"] = "Октябрь"
+L["nov"] = "Ноябрь"
+L["dec"] = "Декабрь"
+
+L["currency-category.gold"] = "Золото"
+L["currency-category.misc"] = "Разное"
+L["currency-category.pvp"] = "Игрок против игрока"
+L["currency-category.dungeonraid"] = "Подземелье и рейд"
+L["currency-category.classic"] = "Classic"
+L["currency-category.tbc"] = "Burning Crusade"
+L["currency-category.wotlk"] = "Wrath of the Lich King"
+L["currency-category.cata"] = "Cataclysm"
+L["currency-category.mop"] = "Mists of Pandaria"
+L["currency-category.wod"] = "Warlords of Draenor"
+L["currency-category.legion"] = "Legion"
+L["currency-category.bfa"] = "Battle for Azeroth"
+L["currency-category.sl"] = "Shadowlands"
+L["currency-category.df"] = "Dragonflight"
+L["currency-category.tww"] = "The War Within"
+
+L["button-next"] = "Следующий"
+L["button-prev"] = "Предыдущий"
+
+L["date"] = "Дата"
+L["amount"] = "Количество"
+L["difference"] = "Разница"
+
+L["no-entries"] = "Нет записей за этот месяц."
+
+L["general-options"] = "Общие параметры"
+L["open-on-login.name"] = "Открыть обзор при входе в систему"
+L["open-on-login.tooltip"] = "Активировать или деактивировать автоматическое открытие обзора золота и валюты при входе в систему."
+
+L["other-options"] = "Другие параметры"
+L["debug.name"] = "Включить режим отладки"
+L["debug.tooltip"] = "Активирует или деактивирует режим отладки."
