6db7da0c64714b97740f7d8165b14290818bc3f6
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-04-21 19:46:03 +0500

    Update RU locale

--- locals/localization.ruRU.lua
+++ locals/localization.ruRU.lua
@@ -6,7 +6,7 @@ local L = GCT.localization
 
 local linkFontColor = "ff66BBFF"
 
-L["addon-name"] = "Gold & Currency Tracker"
+L["addon-name"] = "Трекер золота и валюты"
 
 L["jan"] = "Январь"
 L["feb"] = "Февраль"
@@ -51,18 +51,18 @@ L["difference"] = "Разница"
 
 L["no-entries"] = "Нет записей за этот месяц."
 
-L["minimap-button.tooltip"] = "|c" .. linkFontColor .. "Left-click|r to open the gold and currency overview.\n|c" .. linkFontColor .. "Right-click|r to open the options."
+L["minimap-button.tooltip"] = "|c" .. linkFontColor .. "ЛКМ|r - открыть обзор золота и валюты.\n|c" .. linkFontColor .. "ПКМ|r - открыть настройки."
 
 -- Options
 
-L["options.general"] = "General Options"
-L["options.open-on-login.name"] = "Gold and Currency Overview"
-L["options.open-on-login.tooltip"] = "When this is enabled, the gold and currency overview opens automatically when logging in."
-L["options.minimap-button-hide.name"] = "Minimap Button"
-L["options.minimap-button-hide.tooltip"] = "When this is enabled, the minimap button is displayed."
-L["options.minimap-button-position.name"] = "Position"
-L["options.minimap-button-position.tooltip"] = "Determines the position of the minimap button."
+L["options.general"] = "Общие параметры"
+L["options.open-on-login.name"] = "Обзор золота и валюты"
+L["options.open-on-login.tooltip"] = "Если эта функция включена, обзор золота и валюты открывается автоматически при входе в систему."
+L["options.minimap-button-hide.name"] = "Кнопка миникарты"
+L["options.minimap-button-hide.tooltip"] = "Если эта функция включена, отображается кнопка на миникарте."
+L["options.minimap-button-position.name"] = "Положение"
+L["options.minimap-button-position.tooltip"] = "Определяет положение кнопки миникарты."
 
-L["options.other"] = "Other Options"
-L["options.debug-mode.name"] = "Debug Mode"
-L["options.debug-mode.tooltip"] = "Enabling the debug mode displays additional information in the chat."
+L["options.other"] = "Другие параметры"
+L["options.debug-mode.name"] = "Режим отладки"
+L["options.debug-mode.tooltip"] = "Включение режима отладки отображает дополнительную информацию в чате."
